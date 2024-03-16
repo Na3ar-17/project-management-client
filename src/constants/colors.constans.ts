c35cd700e6766e155fbed515a8e2ea3f3514d1fc
Na3ar-17 <gavruk2017@gmail.com>
2024-03-16 14:07:02 +0100

    added colors to tailwind config

---
+++ src/constants/colors.constans.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: '#1C1C1C',
+  blue: '#268BFF',
+  text: '#FFFFFF',
+  text2: '#C5C5C5',
+  'menu-text': '#B4B4B4',
+  purple: '#972592',
+  green: '#3B6330',
+  'green-light': '#58E598',
+  'light-orange': '#FFC75F',
+  border: '#373737',
+}
