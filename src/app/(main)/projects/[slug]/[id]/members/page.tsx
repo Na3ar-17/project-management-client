1179eb964b29f57d7ed8f7e79b54ff80abd89476
Na3ar-17 <gavruk2017@gmail.com>
2024-05-04 18:08:15 +0200

    implemented fetching members data from the server

--- src/app/(main)/projects/[slug]/[id]/members/page.tsx
+++ src/app/(main)/projects/[slug]/[id]/members/page.tsx
@@ -1,8 +1,14 @@
 import Members from '@/components/display/Members/Members'
 import { NextPage } from 'next'
 
-const page: NextPage = () => {
-  return <Members />
+export interface IProps {
+  params: {
+    id: string
+  }
+}
+
+const page: NextPage<IProps> = ({ params: { id } }) => {
+  return <Members projectId={id} />
 }
 
 export default page
--- src/components/display/Members/Members.tsx
+++ src/components/display/Members/Members.tsx
@@ -1,15 +1,29 @@
+'use client'
 import { NextPage } from 'next'
 import DataTable from './Table/DataTable'
-import { membersData } from '@/data/members.data'
 import { columns } from './Table/Columns/Columns'
 import Heading from '@/components/ui/heading/Heading'
+import { useGetAllMembers } from '@/api/hooks/members/useGetAllMembers'
+import styles from './Members.module.scss'
+interface IProps {
+  projectId: string
+}
 
-const Members: NextPage = () => {
+const Members: NextPage<IProps> = ({ projectId }) => {
+  const { isFetching, isSuccess, membersData } = useGetAllMembers(projectId)
+  if (!isSuccess || !membersData) {
+    // TODO handle error
+    return <div> Error</div>
+  }
   return (
     <div>
       <Heading text="Members" />
       <div className="pt-6">
-        <DataTable data={membersData} columns={columns} />
+        {membersData.length <= 1 ? (
+          <div className={styles['no-members']}>asd</div>
+        ) : (
+          <DataTable data={membersData} columns={columns} />
+        )}
       </div>
     </div>
   )
