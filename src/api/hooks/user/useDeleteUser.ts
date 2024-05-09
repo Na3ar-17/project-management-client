f0bb935dc4858e1a91b5940a414408fafeece841
Na3ar-17 <gavruk2017@gmail.com>
2024-05-09 16:11:00 +0200

    implemented deleting user acoount

--- src/api/hooks/user/useDeleteUser.ts
+++ src/api/hooks/user/useDeleteUser.ts
@@ -1,17 +1,19 @@
 import { userKeys } from '@/api/keys/user.keys'
 import { removeFromStorage } from '@/api/services/auth-toke.service'
 import { userService } from '@/api/services/user.service'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { DASHBOARD_PAGES } from '@/config/pages-url-config'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
 
 export const useDeleteUser = () => {
-  const queryClient = useQueryClient()
+  const { replace } = useRouter()
   const { mutate: deleteUserMutation } = useMutation({
     mutationKey: [userKeys.DELETE],
     mutationFn: () => userService.delete(),
     onSuccess: () => {
+      replace(DASHBOARD_PAGES.AUTH)
       removeFromStorage()
-      queryClient.invalidateQueries()
       toast.success('Account deleted successfully')
     },
   })
