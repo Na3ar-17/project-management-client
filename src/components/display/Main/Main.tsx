7aed097311c6a2160159c99ab8f8ee551686974d
Na3ar-17 <gavruk2017@gmail.com>
2024-05-15 13:57:37 +0200

    created empty main component

---
+++ src/components/display/Main/Main.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import styles from './Main.module.scss'
+
+const Main: NextPage = () => {
+  return <main className={styles.container}>Main</main>
+}
+
+export default Main
